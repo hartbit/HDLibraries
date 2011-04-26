7678a654a3f5c5ad6398459b620196117e42a960
David Hart <david@hart-dev.com>
2011-04-26 15:18:50 +0200

    Added helper functions for transforming orientations into NSString

--- HDKit/HDFunctions.h
+++ HDKit/HDFunctions.h
@@ -6,7 +6,7 @@
 //  Copyright 2011 hart[dev]. All rights reserved.
 //
 
-#import <UIKit/UIKit.h>
+#import "HDFoundation.h"
 
 
 static inline UIColor* UIColorMake(NSUInteger r, NSUInteger g, NSUInteger b)
@@ -34,4 +34,31 @@ static inline CGFloat CGPointDistance(CGPoint first, CGPoint second)
 	CGFloat x = first.x - second.x;
 	CGFloat y = first.y - second.y;
 	return sqrt(x * x + y * y);
+}
+
+static inline NSString* NSStringFromDeviceOrientation(UIDeviceOrientation orientation)
+{
+	switch (orientation)
+	{
+		case UIDeviceOrientationUnknown: return @"UIDeviceOrientationUnknown";
+		case UIDeviceOrientationPortrait: return @"UIDeviceOrientationPortrait";
+		case UIDeviceOrientationPortraitUpsideDown: return @"UIDeviceOrientationPortraitUpsideDown";
+		case UIDeviceOrientationLandscapeLeft: return @"UIDeviceOrientationLandscapeLeft";
+		case UIDeviceOrientationLandscapeRight: return @"UIDeviceOrientationLandscapeRight";
+		case UIDeviceOrientationFaceUp: return @"UIDeviceOrientationFaceUp";
+		case UIDeviceOrientationFaceDown: return @"UIDeviceOrientationFaceDown";
+		default: HDCFail(@"Unkown device orientation", HDFailureLevelError); return nil;
+	}
+}
+
+static inline NSString* NSStringFromInterfaceOrientation(UIInterfaceOrientation orientation)
+{
+	switch (orientation)
+	{
+		case UIInterfaceOrientationPortrait: return @"UIInterfaceOrientationPortrait";
+		case UIInterfaceOrientationPortraitUpsideDown: return @"UIInterfaceOrientationPortraitUpsideDown";
+		case UIInterfaceOrientationLandscapeLeft: return @"UIInterfaceOrientationLandscapeLeft";
+		case UIInterfaceOrientationLandscapeRight: return @"UIInterfaceOrientationLandscapeRight";
+		default: HDCFail(@"Unkown interface orientation", HDFailureLevelError); return nil;
+	}
 }
